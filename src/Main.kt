4a39733bdf1a739e94ad2bb56c8ea7ed80eba074
Axel Briano <axel.briano@pegipegi.com>
2022-02-26 13:48:46 +0700

    base template request buffer

--- src/Main.kt
+++ src/Main.kt
@@ -1,4 +1,31 @@
+import java.io.BufferedInputStream
+import java.io.BufferedOutputStream
+import java.net.Socket
+import java.util.concurrent.ExecutorService
 
 fun main() {
-    println("hello mom!")
+    val socket = Socket("monta.if.its.ac.id", 80)
+
+    val bufferIn = BufferedInputStream(socket.getInputStream())
+    val bufferOut = BufferedOutputStream(socket.getOutputStream())
+
+    println("gonna  write to buferOut")
+    bufferOut.write((
+            "GET / HTTP/1.1\r\n" +
+            "Host: monta.if.its.ac.id\r\n\r\n"
+            ).toByteArray())
+    bufferOut.flush()
+
+    println("waiting to readAllBytes")
+
+    var response = ""
+    var byteArray = bufferIn.readNBytes(100)
+
+    while (byteArray.isNotEmpty()) {
+        response += String(byteArray)
+        byteArray = bufferIn.readNBytes(100)
+    }
+    println(response)
+    println("read buffer complete")
+    socket.close()
 }
